762f72647fb458b013c7a1cd9a7660b8a0fa2496
xiyanggudao <1766500102@qq.com>
2017-08-02 00:02:51 +0800

    - 命名规范修改 - 使用tab缩进

--- test/__main__.py
+++ test/__main__.py
@@ -3,16 +3,13 @@ import unittest
 
 
 def load_tests(loader, standard_tests, pattern):
-    # top level directory cached on loader instance
-    this_dir = os.path.dirname(__file__)
-    pattern = pattern or "Test*.py"
-    # We are inside unittest.test, so the top-level is two notches up
-    top_level_dir = os.path.dirname(os.path.dirname(this_dir))
-    package_tests = loader.discover(start_dir=this_dir, pattern=pattern,
-                                    top_level_dir=top_level_dir)
-    standard_tests.addTests(package_tests)
-    return standard_tests
+	testsDir = os.path.dirname(__file__)
+	pattern = pattern or "Test*.py"
+	projectDir = os.path.dirname(os.path.dirname(testsDir))
+	package_tests = loader.discover(start_dir=testsDir, pattern=pattern, top_level_dir=projectDir)
+	standard_tests.addTests(package_tests)
+	return standard_tests
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
