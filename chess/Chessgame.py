e4cade2ddcfde2f8f9dcf69ba20e20ac3d20291c
xiyanggudao <1766500102@qq.com>
2017-08-03 00:45:42 +0800

    ...

--- chess/Chessgame.py
+++ chess/Chessgame.py
@@ -30,6 +30,31 @@ class ChessmanOnBoard:
 	def color(self):
 		return Chessman.getColor(self.__identifier)
 
+# 走棋数据，提供外面访问走棋历史的接口
+class Move:
+
+	def __init__(self, moveChessman, fromPos, toPos, ateChessman):
+		self.__fromPos = fromPos
+		self.__toPos = toPos
+		self.__moveChessman = moveChessman
+		self.__ateChessman = ateChessman
+
+	@property
+	def fromPos(self):
+		return self.__fromPos
+
+	@property
+	def toPos(self):
+		return self.__toPos
+
+	@property
+	def moveChessman(self):
+		return self.__moveChessman
+
+	@property
+	def ateChessman(self):
+		return self.__ateChessman
+
 class Chessgame:
 
 	def __init__(self):
