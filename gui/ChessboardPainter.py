c09cd8cd80cc684f3c3358c07e08a8dfd28d4079
xiyanggudao <1766500102@qq.com>
2017-08-06 19:44:42 +0800

    - 图元内存泄漏问题

--- gui/ChessboardPainter.py
+++ gui/ChessboardPainter.py
@@ -22,14 +22,15 @@ class ChessboardPainter:
 		self.__chessSize = chessSize
 
 	def clearBoard(self, width, height):
+		self.__canvas.delete('x') # 不清理会有内存泄漏
 		self.__canvas.create_rectangle(0, 0, width, height, fill=self.__boardColor)
 
 	# (x, y) 是左上角的坐标
 	def drawRectangle(self, x, y, width, height, boardWidth):
-		self.__canvas.create_rectangle(x, y, x+width, y+height, width=boardWidth)
+		self.__canvas.create_rectangle(x, y, x+width, y+height, width=boardWidth,tag='x')
 
 	def drawLine(self, x1, y1, x2, y2, width):
-		self.__canvas.create_line(x1, y1, x2, y2, width=width)
+		self.__canvas.create_line(x1, y1, x2, y2, width=width,tag='x')
 
 	# (x, y) 是棋子中心的坐标
 	def drawChess(self, x, y, name):
@@ -40,12 +41,12 @@ class ChessboardPainter:
 		textSize = int(0.7*self.__chessSize)
 		font = '-family 楷体 -size %d'%textSize
 
-		self.__canvas.create_oval(left, top, right, bottom, fill=self.__chessBaseColor)
-		self.__canvas.create_text((x, y), text=name, fill=self.__chessColor, font=font)
+		self.__canvas.create_oval(left, top, right, bottom, fill=self.__chessBaseColor,tag='x')
+		self.__canvas.create_text((x, y), text=name, fill=self.__chessColor, font=font,tag='x')
 
 	def drawText(self, x, y, text):
 		textSize = int(0.7 * self.__chessSize)
 		font = '-family 隶书 -size %d' % textSize
-		self.__canvas.create_text((x, y), text=text, fill='black', font=font)
+		self.__canvas.create_text((x, y), text=text, fill='black', font=font,tag='x')
 
 
